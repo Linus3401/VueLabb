13e133a8edf643cc8cad3830e99a11a51ec11ba1
Linus3401 <112473172+Linus3401@users.noreply.github.com>
2023-02-11 17:16:47 +0100

    2

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/base.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(bootstrap)
 
 app.mount('#app')
