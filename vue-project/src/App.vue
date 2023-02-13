9b53f7db2bd4276f6553b56c42f23e98729623d0
Linus3401 <112473172+Linus3401@users.noreply.github.com>
2023-02-13 09:07:53 +0100

    3

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,13 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+    import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-
-
-  <RouterView />
+    <NavBar />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ vue-project/src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {RouterLink, RouterView} from 'vue-router'
+</script>
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <RouterLink class="navbar-brand" to="/">Fågelholk</RouterLink>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="/about">About</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<RouterView/>
+</template>
+<style scoped>
+
+</style>
