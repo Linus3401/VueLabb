e5eec66a2b2125a0eab7d9dd72c02d13c0707865
Linus3401 <112473172+Linus3401@users.noreply.github.com>
2023-02-17 13:00:32 +0100

    oklart

---
+++ vue-project/src/components/BirdTypes.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card" style="width: 18rem">
+        <div class="card-body">
+            <h5 class="card-title">{{ info.number }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{ info.name }}</h6>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        info:{
+            required: true,
+            type: Object
+        }
+    }
+}
+</script>
+<style></style>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,7 +1,38 @@
 <script setup>
-
+    import BirdTypes from '../components/BirdTypes.vue'
 </script>
 
 <template>
-
+    <h1 class="bird-title">{{ message }}</h1>
+    <div id="info">
+        <div class="container text-center">
+            <div class="row align-items-start">
+                <bird v-for="bird in birds" ::key="bird.id" :info="bird" />
+                <Bird />
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Fåglar i sverige!',
+                birds: []
+            }
+        },
+
+        components: { 'bird': BirdTypes },
+        mounted() {
+            this.fetchData()
+        },
+        methods: {
+            async fetchData() {
+                const res = await fetch('bird.json')
+                const result = await res.json()
+                this.birds = result
+            }
+        }
+    }
+</script>
+<style scoped></style>
